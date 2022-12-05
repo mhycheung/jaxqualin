05c37318b8aa809b5bd32720540d102d62d8011b
Mark  Ho-Yeuk Cheung <mcheung1@login01.expanse.sdsc.edu>
2022-12-05 11:01:36 -0800

    run with SXS0209 to SXS0305

--- SXS_data_frame.py
+++ SXS_data_frame.py
@@ -1,4 +1,6 @@
 from postprocess import *
 
-SXS_num_list = [str(SXS_num) for SXS_num in range(1419,1510)]
-create_data_frame(SXS_num_list, [8, 10], "run6")
+SXS_num_list_1 = [str(SXS_num) for SXS_num in range(1419,1510)]
+SXS_num_list_2 = ["0" + str(SXS_num) for SXS_num in range(209,306)]
+SXS_num_list = SXS_num_list_1 + SXS_num_list_2
+create_data_frame(SXS_num_list, [5, 6, 7, 8, 9, 10], "2_run1")
--- SXS_fit.py
+++ SXS_fit.py
@@ -13,11 +13,11 @@ from ModeSelection import *
 import sys
 
 SXS_num = sys.argv[1]
-mode_search_complete = ModeSearchAllFreeVaryingNSXSAllRelevant(SXS_num, N_list = [8, 10], 
+mode_search_complete = ModeSearchAllFreeVaryingNSXSAllRelevant(SXS_num, N_list = [5, 6, 7, 8, 9, 10], 
                                                               load_pickle = False
                                                               )
 mode_search_complete.do_all_searches()
 Mf = mode_search_complete.relevant_lm_mode_searcher_varying_N[0].M
 af = mode_search_complete.relevant_lm_mode_searcher_varying_N[0].a
 predicted_qnm_list = []
-plot_relevant_mode_search_full(mode_search_complete, predicted_qnm_list = predicted_qnm_list, postfix_string="run2")
+plot_relevant_mode_search_full(mode_search_complete, predicted_qnm_list = predicted_qnm_list, postfix_string="run3")
